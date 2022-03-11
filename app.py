2cd8969673c2f3072d214d9b422daf4ce06166ee
Matt Powers <mrpowe07@outlook.com>
2022-03-10 21:09:48 -0600

    Jsonify welcome()

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route('/hello/<string:name>', methods=['GET', 'POST'])
 def welcome(name):
-    return f"<h1>Hello, {name}</h1>"
+    return json.dumps( {'name' : name }), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8005)
