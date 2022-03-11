3487884977ef1b487a53f19b8874769848ca7374
Matt Powers <mrpowe07@outlook.com>
2022-03-10 21:00:28 -0600

    Change to using flask

--- hello.py
+++ hello.py
@@ -1,12 +1,8 @@
 import sys
 import time
 
-def main():
+def hello():
     args = sys.argv
     while True:
         print("hello " + args[1] if len(args) > 1 else "balls")
         time.sleep(3)
-
-
-if __name__ == "__main__":
-    main()
