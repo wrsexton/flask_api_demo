823814032a547ad872725e58371c175a3dbe5030
Matt Powers <mrpowe07@outlook.com>
2022-03-10 20:33:45 -0600

    Add argv to hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
+import sys
 import time
 
-def main(args):
+def main():
+    args = sys.argv
     while True:
-        print("hello " + args[0] if len(args) else "balls")
+        print("hello " + args[1] if len(args) > 1 else "balls")
         time.sleep(3)
+
 
 if __name__ == "__main__":
     main()
