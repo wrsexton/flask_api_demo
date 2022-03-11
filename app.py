d5d109c32dea98673dac47e47ba230d1b368a192
Matt Powers <mrpowe07@outlook.com>
2022-03-10 21:04:59 -0600

    Add name to /hello/ route

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello/', methods=['GET', 'POST'])
-def welcome():
-    return "Hello World!"
+@app.route('/hello/<string:name>', methods=['GET', 'POST'])
+def welcome(name):
+    return f"<h1>Hello, {name}</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8005)
